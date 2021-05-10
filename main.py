ced0bf55eb453a49986a4016d62f8289ffd038c9
Takahashi Katsuyuki <58053010+kkml4220@users.noreply.github.com>
2021-05-10 22:33:31 +0900

    fix main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import roboter.controller.conversation
-roboter.controller.conversation.talk_about_restaurant()
+
+
+def main():
+    roboter.controller.conversation.talk_about_restaurant()
+
+
+if __name__ == '__main__':
+    main()
